e77ed027446dc064822b275ebc870faf3009c368
Zaheer872004 <zaheerkhan872004.@gmail.com>
2023-10-29 11:01:05 +0530

    first save

---
+++ intro.js
@@ -0,0 +1,7 @@
+// console.log("hello world!");
+console.log(typeof 12);
+console.log(typeof true);
+console.log(typeof typeof 12);
+console.log(typeof typeof true);
+console.log(typeof typeof 34);
+console.log(typeof typeof zaheer);
